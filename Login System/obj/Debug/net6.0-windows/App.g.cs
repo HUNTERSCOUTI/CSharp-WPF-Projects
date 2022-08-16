489eb89700dda275b330993337adfe2fb46a54b4
Zilas Jørgensen <zila0082@elev.techcollege.dk>
2022-08-16 12:12:28 +0200

    New Desktop Inital

--- Login System/obj/Debug/net6.0-windows/App.g.cs
+++ Login System/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace Login_System {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.7.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Login_System {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.7.0")]
         public static void Main() {
             Login_System.App app = new Login_System.App();
             app.InitializeComponent();
