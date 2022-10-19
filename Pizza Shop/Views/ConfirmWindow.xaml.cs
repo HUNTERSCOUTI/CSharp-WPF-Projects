cd6e4365af669576b6cb4887ceab78cdb113f8f4
HUNTERSCOUTI <39131351+HUNTERSCOUTI@users.noreply.github.com>
2022-10-19 20:54:20 +0200

    Confirm Window Initial

---
+++ Pizza Shop/Views/ConfirmWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pizza_Shop.Views
+{
+    /// <summary>
+    /// Interaction logic for ConfirmWindow.xaml
+    /// </summary>
+    public partial class ConfirmWindow : Window
+    {
+        public ConfirmWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYes(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnNo(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
