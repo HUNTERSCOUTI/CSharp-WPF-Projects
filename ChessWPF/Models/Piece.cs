1f21d6cf96952a7df922b0c91aa01784f6d6bb57
Zilas Jørgensen <Zilas2004@gmail.com>
2023-05-25 14:59:27 +0200

    Chess Addition

---
+++ ChessWPF/Models/Piece.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ChessWPF.Models
+{
+    public class Piece
+    {
+        public string PieceName { get; set; }
+        public bool IsWhite { get; set; }
+        public Square CurrentSquare { get; set; }
+        public Square[] PossibleMoves { get; set; }
+        public Image PieceImage { get; set; }
+
+        public Piece() { }
+        public Piece(string name, bool isWhite, Square currentSquare, Square[] possibleMoves, Image image)
+        {
+            PieceName = name;
+            IsWhite = isWhite;
+            CurrentSquare = currentSquare;
+            PossibleMoves = possibleMoves;
+            PieceImage = image;
+        }
+
+        //Make on mouse click
+
+    }
+}
---
+++ ChessWPF/Models/Square.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessWPF.Models
+{
+    public class Square
+    {
+        public string SquareName { get; set; }
+
+        //Make on mouse click
+    }
+}
